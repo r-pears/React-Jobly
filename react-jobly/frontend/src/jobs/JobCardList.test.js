97e720928b510bd75b6ac0290a6033b5d8d4158b
Ryan <ryjen.pears@gmail.com>
2022-08-03 23:27:55 +0200

    test for job card list added

--- react-jobly/frontend/src/jobs/JobCardList.test.js
+++ react-jobly/frontend/src/jobs/JobCardList.test.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { UserProvider } from '../testUtils';
 import JobCardList from './JobCardList';
 
-it('matches snapshot', () => {
-  const { asFragment } = render(
-    <UserProvider>
-      <JobCardList/>
-    </UserProvider>
+it('renders without crashing', () => {
+  render(
+    <JobCardList/>
   );
-  expect(asFragment()).toMatchSnapshot();
 });
